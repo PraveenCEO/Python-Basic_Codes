d29cd670af3df52fb26120dee394e01cf74d0b34
Praveen V <praveenvignesh2001@gmail.com>
2023-07-03 19:48:30 +0530

    Factorial.py

--- Factorial.py
+++ Factorial.py
@@ -1,8 +1,12 @@
-# n=int(input())
-# fact=1
-# for i in range(1,n+1):
-#     fact=fact*i
-# print(fact)
+# First method
+n=int(input())
+ fact=1
+ for i in range(1,n+1):
+    fact=fact*i
+print(fact)
+
+# ------------------------
+# Second method
 def fact(n):
     factt=1
     for i in range(1,n+1):
